e74c80dfb97b3fc0d283149099c32c4ce3178a5c
kiwan <drifterz303@naver.com>
2015-01-15 11:28:49 +0900

    php address change

--- src/Variable/GlobalVariable.java
+++ src/Variable/GlobalVariable.java
@@ -5,12 +5,13 @@ package Variable;
 
 public class GlobalVariable {
 	//public final static String url = "http://211.39.253.201/game/";
-	public final static String url = "http://cra16.handong.edu/Dabarun/";
+	public final static String url = "http://cra16.handong.edu/Dabarun/user";
 	
-	public final static String login = url + "login.php";
+	public final static String login = url + "userLogin.php";
 	
 	public final static String push = url + "push_all.php";
-
+	
+	public final static String getCropList= url + "getCropList.php";
 	public final static String getPoint = url + "getPoint.php";
 	public final static String setPoint = url + "setPoint.php";
 	
